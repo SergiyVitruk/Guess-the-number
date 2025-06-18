92534e04dcc571cdd562173d3be90f207e884b06
SergiyVitruk <vitruk.sergiy@gmail.com>
2025-06-18 23:37:35 +0300

    add comments

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
+# import section
 import random
 
+# function section
 def guess_the_number():
     lower_bound = 1
     upper_bound = 100
     guessed_number = random.randint(lower_bound, upper_bound)
     max_attempts = 7
 
+    # input section
     print(f"Вітаю! Я загадав число від {lower_bound} до {upper_bound}. Спробуй вгадати його за {max_attempts} спроб.")
 
+    # cycle section
     for attempt in range(1, max_attempts + 1):
         try:
             guess = int(input(f"Введіть ваше припущення: "))
@@ -15,6 +19,7 @@ def guess_the_number():
             print("Будь ласка, введіть ціле число.")
             continue
 
+        # condition section
         if guess < guessed_number:
             print("Занадто маленьке!")
         elif guess > guessed_number:
@@ -25,5 +30,6 @@ def guess_the_number():
     else:
         print(f"На жаль, ви не вгадали. Загадане число було: {guessed_number}")
 
+# function call
 if __name__ == "__main__":
     guess_the_number()
